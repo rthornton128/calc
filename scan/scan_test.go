658594fddd34e4a57668d5d502e0e6d67c07e7ad
Rob Thornton <rthornton128@gmail.com>
2014-04-04 16:39:58 -0700

    Import scan tests

---
+++ scan/scan_test.go
@@ -0,0 +1,50 @@
+package scan_test
+
+import (
+  "testing"
+
+  "github.com/rthornton128/calc1/scan"
+  "github.com/rthornton128/calc1/token"
+)
+
+var src = "(+ 2 (- 4 1) (* 6 5) (% 10 2) (/ 9 3))"
+
+func TestScan(t *testing.T) {
+  var expected = []token.Token{
+    token.LPAREN,
+    token.ADD,
+    token.INTEGER,
+    token.LPAREN,
+    token.SUB,
+    token.INTEGER,
+    token.INTEGER,
+    token.RPAREN,
+    token.LPAREN,
+    token.MUL,
+    token.INTEGER,
+    token.INTEGER,
+    token.RPAREN,
+    token.LPAREN,
+    token.REM,
+    token.INTEGER,
+    token.INTEGER,
+    token.RPAREN,
+    token.LPAREN,
+    token.QUO,
+    token.INTEGER,
+    token.INTEGER,
+    token.RPAREN,
+    token.RPAREN,
+  }
+
+  s := &scan.Scanner{}
+  s.Init(src)
+
+  _, tok, pos := s.Scan()
+  for i := 0; tok != token.EOF; i++ {
+    if tok != expected[i] {
+      t.Fatal(pos, "Expected:", expected[i], "Got:", tok)
+    }
+    _, tok, pos = s.Scan()
+  }
+}
