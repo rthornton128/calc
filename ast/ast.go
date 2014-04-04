91e6229e3eedc87c038488570256249d482cf8ab
Rob Thornton <rthornton128@gmail.com>
2014-04-04 09:48:08 -0700

    Import ast

---
+++ ast/ast.go
@@ -0,0 +1,42 @@
+package ast
+
+import (
+	"github.com/rthornton128/calc1/token"
+)
+
+type Node interface {
+	Pos() token.Pos
+	End() token.Pos
+}
+
+type Expr interface {
+	Node
+	exprNode()
+}
+
+type BasicLit struct {
+	LitPos token.Pos
+	Kind   token.Token
+	Lit    string
+}
+
+type BinaryExpr struct {
+	Expression
+	Op    token.Token
+	OpPos token.Pos
+	List  []Expr
+}
+
+type Expression struct {
+	Opening token.Pos
+	Closing token.Pos
+}
+
+func (b *BasicLit) Pos() token.Pos   { return b.LitPos }
+func (e *Expression) Pos() token.Pos { return e.Opening }
+
+func (b *BasicLit) End() token.Pos   { return b.LitPos + token.Pos(len(b.Lit)) }
+func (e *Expression) End() token.Pos { return e.Closing }
+
+func (b *BasicLit) exprNode()   {}
+func (e *Expression) exprNode() {}
---
+++ ast/ast_test.go
@@ -0,0 +1,50 @@
+package ast_test
+
+import (
+	"testing"
+
+	"github.com/rthornton128/calc1/ast"
+	"github.com/rthornton128/calc1/token"
+)
+
+func TestBasicLit(t *testing.T) {
+	b := &ast.BasicLit{
+		LitPos: token.Pos(1),
+		Kind:   token.INTEGER,
+		Lit:    "24",
+	}
+	pos, end := token.Pos(1), token.Pos(3)
+	if b.Pos() != pos || b.End() != end {
+		t.Fatal("Expected:", pos, end, "Got:", b.Pos(), b.End())
+	}
+}
+
+func TestBinaryExpr(t *testing.T) {
+	// (+ 3 5)
+	x := &ast.BasicLit{
+		LitPos: token.Pos(4),
+		Kind:   token.INTEGER,
+		Lit:    "3",
+	}
+	y := &ast.BasicLit{
+		LitPos: token.Pos(6),
+		Kind:   token.INTEGER,
+		Lit:    "5",
+	}
+	b := &ast.BinaryExpr{
+		Expression: ast.Expression{
+			Opening: token.Pos(1),
+			Closing: token.Pos(7),
+		},
+		Op:    token.ADD,
+		OpPos: token.Pos(2),
+		List:  []ast.Expr{x, y},
+	}
+
+	if b.Pos() != token.Pos(1) {
+		t.Fatal("BinaryExpr: Expected: 1 Got:", b.Pos())
+	}
+	if b.End() != token.Pos(7) {
+		t.Fatal("BinaryExpr: Expected: 7 Got:", b.End())
+	}
+}
