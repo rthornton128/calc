d8f08974f2df708ddb88628b628de078a6065434
Rob Thornton <rthornton128@gmail.com>
2014-04-10 10:17:16 -0700

    Fix Issue #5 - add user license

--- comp/comp_test.go
+++ comp/comp_test.go
@@ -1,16 +1,23 @@
+// Copyright (c) 2014, Rob Thornton
+// All rights reserved.
+// This source code is governed by a Simplied BSD-License. Please see the
+// LICENSE included in this distribution for a copy of the full license
+// or, if one is not included, you may also find a copy at
+// http://opensource.org/licenses/BSD-2-Clause
+
 package comp_test
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/rthornton128/calc1/comp"
+	"github.com/rthornton128/calc1/comp"
 )
 
 func TestCompileFile(t *testing.T) {
-  comp.CompileFile("c1", "(+ 5 3)")
-  comp.CompileFile("c2", "(- 5 3)")
-  comp.CompileFile("c3", "(- 10 2 4)")
-  comp.CompileFile("c4", "(+ (+ 2 3) 3)")
-  comp.CompileFile("c5", "(% (+ 2 8) 2)")
-  comp.CompileFile("c6", "(/ (* 4 3) (+ 3 3))")
+	comp.CompileFile("c1", "(+ 5 3)")
+	comp.CompileFile("c2", "(- 5 3)")
+	comp.CompileFile("c3", "(- 10 2 4)")
+	comp.CompileFile("c4", "(+ (+ 2 3) 3)")
+	comp.CompileFile("c5", "(% (+ 2 8) 2)")
+	comp.CompileFile("c6", "(/ (* 4 3) (+ 3 3))")
 }
--- scan/scan_test.go
+++ scan/scan_test.go
@@ -1,50 +1,57 @@
+// Copyright (c) 2014, Rob Thornton
+// All rights reserved.
+// This source code is governed by a Simplied BSD-License. Please see the
+// LICENSE included in this distribution for a copy of the full license
+// or, if one is not included, you may also find a copy at
+// http://opensource.org/licenses/BSD-2-Clause
+
 package scan_test
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/rthornton128/calc1/scan"
-  "github.com/rthornton128/calc1/token"
+	"github.com/rthornton128/calc1/scan"
+	"github.com/rthornton128/calc1/token"
 )
 
 var src = "(+ 2 (- 4 1) (* 6 5) (% 10 2) (/ 9 3)); comment\n"
 
 func TestScan(t *testing.T) {
-  var expected = []token.Token{
-    token.LPAREN,
-    token.ADD,
-    token.INTEGER,
-    token.LPAREN,
-    token.SUB,
-    token.INTEGER,
-    token.INTEGER,
-    token.RPAREN,
-    token.LPAREN,
-    token.MUL,
-    token.INTEGER,
-    token.INTEGER,
-    token.RPAREN,
-    token.LPAREN,
-    token.REM,
-    token.INTEGER,
-    token.INTEGER,
-    token.RPAREN,
-    token.LPAREN,
-    token.QUO,
-    token.INTEGER,
-    token.INTEGER,
-    token.RPAREN,
-    token.RPAREN,
-  }
+	var expected = []token.Token{
+		token.LPAREN,
+		token.ADD,
+		token.INTEGER,
+		token.LPAREN,
+		token.SUB,
+		token.INTEGER,
+		token.INTEGER,
+		token.RPAREN,
+		token.LPAREN,
+		token.MUL,
+		token.INTEGER,
+		token.INTEGER,
+		token.RPAREN,
+		token.LPAREN,
+		token.REM,
+		token.INTEGER,
+		token.INTEGER,
+		token.RPAREN,
+		token.LPAREN,
+		token.QUO,
+		token.INTEGER,
+		token.INTEGER,
+		token.RPAREN,
+		token.RPAREN,
+	}
 
-  var s scan.Scanner
-  s.Init(token.NewFile("", src), src)
+	var s scan.Scanner
+	s.Init(token.NewFile("", src), src)
 
-  _, tok, pos := s.Scan()
-  for i := 0; tok != token.EOF; i++ {
-    if tok != expected[i] {
-      t.Fatal(pos, "Expected:", expected[i], "Got:", tok)
-    }
-    _, tok, pos = s.Scan()
-  }
+	_, tok, pos := s.Scan()
+	for i := 0; tok != token.EOF; i++ {
+		if tok != expected[i] {
+			t.Fatal(pos, "Expected:", expected[i], "Got:", tok)
+		}
+		_, tok, pos = s.Scan()
+	}
 }
