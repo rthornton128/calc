b252d2955b73263becff3c860dfd66ad5030079e
Rob Thornton <rthornton128@gmail.com>
2014-05-07 21:19:55 -0700

    refactor parse tests and add test for var expression

--- parse/parse_test.go
+++ parse/parse_test.go
@@ -19,15 +19,12 @@ const (
 	BINARY
 	IDENT
 	FILE
+	VAR
 )
 
-func TestParseFileBasic(t *testing.T) {
-	f := parse.ParseFile("test.calc", "(+ 3 5)")
+func nodeTest(types []int, t *testing.T) func(node ast.Node) {
 	i := 0
-
-	var types = []int{FILE, IDENT, BINARY, BASIC, BASIC}
-
-	ast.Walk(f, func(node ast.Node) {
+	return func(node ast.Node) {
 		switch node.(type) {
 		case *ast.File:
 			if types[i] != FILE {
@@ -45,43 +42,48 @@ func TestParseFileBasic(t *testing.T) {
 			if types[i] != IDENT {
 				t.Fatal("Walk index:", i, "Expected:", types[i], "Got:", IDENT)
 			}
+		case *ast.VarExpr:
+			if types[i] != VAR {
+				t.Fatal("Walk index:", i, "Expected:", types[i], "Got:", IDENT)
+			}
 		default:
 			t.Fatal("Walk index:", i, "Expected:", types[i], "Got: Unknown")
 		}
 		i++
-	})
+	}
+}
+
+func TestParseFileBasic(t *testing.T) {
+	var types = []int{FILE, IDENT, BINARY, BASIC, BASIC}
+
+	f := parse.ParseFile("test.calc", "(+ 3 5)")
+	ast.Walk(f, nodeTest(types, t))
 }
 
 func TestParseNested(t *testing.T) {
-	f := parse.ParseFile("test.calc", ";comment\n(+ (/ 9 3) 5 (- 3 1))")
-	i := 0
-
 	var types = []int{FILE, IDENT, BINARY, BINARY, BASIC, BASIC, BASIC, BINARY,
 		BASIC, BASIC}
+	f := parse.ParseFile("test.calc", ";comment\n(+ (/ 9 3) 5 (- 3 1))")
+	ast.Walk(f, nodeTest(types, t))
+}
 
-	ast.Walk(f, func(node ast.Node) {
-		switch node.(type) {
-		case *ast.File:
-			if types[i] != FILE {
-				t.Fatal("Walk index:", i, "Expected:", types[i], "Got:", FILE)
-			}
-		case *ast.BasicLit:
-			if types[i] != BASIC {
-				t.Fatal("Walk index:", i, "Expected:", types[i], "Got:", BASIC)
-			}
-		case *ast.BinaryExpr:
-			if types[i] != BINARY {
-				t.Fatal("Walk index:", i, "Expected:", types[i], "Got:", BINARY)
-			}
-		case *ast.Ident:
-			if types[i] != IDENT {
-				t.Fatal("Walk index:", i, "Expected:", types[i], "Got:", IDENT)
-			}
-		default:
-			t.Fatal("Walk index:", i, "Expected:", types[i], "Got: Unknown")
-		}
-		i++
-	})
+func TestParseVar(t *testing.T) {
+	var types = []int{FILE, IDENT, VAR, IDENT, IDENT, BASIC}
+	f := parse.ParseFile("test.calc", "(var a 5 int)")
+	ast.Walk(f, nodeTest(types, t))
+
+	types = []int{FILE, IDENT, VAR, IDENT, IDENT}
+	f = parse.ParseFile("test.calc", "(var a int)")
+	ast.Walk(f, nodeTest(types, t))
+
+	types = []int{FILE, IDENT, VAR, IDENT, BASIC}
+	f = parse.ParseFile("test.calc", "(var a 5)")
+	ast.Walk(f, nodeTest(types, t))
+
+	f = parse.ParseFile("test.calc", "(var a)")
+	if f != nil {
+		t.Fatal("Parser returned valid object but expected nil")
+	}
 }
 
 func TestExpectFail(t *testing.T) {
