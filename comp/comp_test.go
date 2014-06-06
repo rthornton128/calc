5d525f7f3c64cd9f3e0880304ca7f73a2765fdd1
Rob Thornton <rthornton128@gmail.com>
2014-06-06 14:03:56 -0700

    update compiler tests

--- comp/comp_test.go
+++ comp/comp_test.go
@@ -8,9 +8,12 @@
 package comp_test
 
 import (
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/rthornton128/calc/comp"
@@ -25,30 +28,38 @@ func init() {
 	}
 }
 
-func TestInteger(t *testing.T) {
-	test_handler(t, "42", "42")
-}
-
 func TestSimpleExpression(t *testing.T) {
-	test_handler(t, "(+ 5 3)", "8")
+	test_handler(t, "(decl main int (+ 5 3))", "8")
 }
 
 func TestSimpleExpressionWithComments(t *testing.T) {
-	test_handler(t, ";comment 1\n(* 5 3); comment 2", "15")
+	test_handler(t, ";comment 1\n(decl main int (* 5 3)); comment 2", "15")
 }
 
 func TestComplexExpression(t *testing.T) {
-	test_handler(t, "(- (* 9 (+ 2 3)) (+ (/ 20 (% 15 10)) 1))", "40")
+	test_handler(t, "(decl main int (- (* 9 (+ 2 3)) (+ (/ 20 (% 15 10)) 1)))",
+		"40")
+}
+
+func TestVarExpression(t *testing.T) {
+	test_handler(t, "(decl main int ((var (= a 5)) a))", "5")
 }
 
 func test_handler(t *testing.T, src, expected string) {
 	defer tearDown()
 
-	comp.CompileFile("test", src)
+	err := ioutil.WriteFile("test.calc", []byte(src), os.ModePerm)
+	if err != nil {
+		t.Fatal(err)
+	}
+	comp.CompileFile("test.calc")
+	os.Remove("test.calc")
 
+	runpath, _ := filepath.Abs("../runtime")
+	runlib := filepath.Join(runpath, "runtime.a")
 	out, err := exec.Command("gcc"+ext, "-Wall", "-Wextra", "-std=c99",
-		"--output=test"+ext, "test.c").CombinedOutput()
-
+		"-I", runpath, "--output=test"+ext,
+		"test.c", runlib).CombinedOutput()
 	if err != nil {
 		t.Log(string(out))
 		t.Fatal(err)
@@ -61,10 +72,10 @@ func test_handler(t *testing.T, src, expected string) {
 	default:
 		output, err = exec.Command("./test").Output()
 	}
+	output = []byte(strings.TrimSpace(string(output)))
+	t.Log("len output:", len(output))
+	t.Log("len expected:", len(expected))
 
-	if err != nil {
-		t.Fatal(err)
-	}
 	if string(output) != expected {
 		t.Fatal("For " + src + " expected " + expected + " got " + string(output))
 	}
