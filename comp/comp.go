e51ab08f402b4221e57c84299a1d0d7e6d073a65
Rob Thornton <rthornton128@gmail.com>
2014-06-12 15:07:10 -0700

    add doc strings to code generator

--- comp/comp.go
+++ comp/comp.go
@@ -5,6 +5,8 @@
 // or, if one is not included, you may also find a copy at
 // http://opensource.org/licenses/BSD-2-Clause
 
+// Package comp comprises the code generation portion of the Calc
+// programming language
 package comp
 
 import (
@@ -28,6 +30,9 @@ type compiler struct {
 	topScope *ast.Scope
 }
 
+// CompileFile generates a C source file for the corresponding file
+// specified by path. The .calc extension for the filename in path is
+// replaced with .c for the C source output.
 func CompileFile(path string) {
 	var c compiler
 
@@ -55,6 +60,9 @@ func CompileFile(path string) {
 	}
 }
 
+// CompileDir generates C source code for the Calc sources found in the
+// directory specified by path. The C source file uses the same name as
+// directory rather than any individual file.
 func CompileDir(path string) {
 	fs := token.NewFileSet()
 	pkg, err := parse.ParseDir(fs, path)
@@ -81,6 +89,8 @@ func CompileDir(path string) {
 
 /* Utility */
 
+// Error adds an error to the compiler at the given position. The remaining
+// arguments are used to generate the error message.
 func (c *compiler) Error(pos token.Pos, args ...interface{}) {
 	c.errors.Add(c.fset.Position(pos), args...)
 }
