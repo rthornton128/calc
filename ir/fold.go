1d26202c14526b820998a19486ddcc17f0dc9113
Rob Thornton <rthornton128@gmail.com>
2015-11-14 15:12:08 -0800

    add license info

--- ir/fold.go
+++ ir/fold.go
@@ -1,3 +1,10 @@
+// Copyright (c) 2015, Rob Thornton
+// All rights reserved.
+// This source code is governed by a Simplied BSD-License. Please see the
+// LICENSE included in this distribution for a copy of the full license
+// or, if one is not included, you may also find a copy at
+// http://opensource.org/licenses/BSD-2-Clause
+
 package ir
 
 import "github.com/rthornton128/calc/token"
